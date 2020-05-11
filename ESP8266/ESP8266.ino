7eeb4b9729f929e381f59fbc89a43f4a2008dfd5
Muhammad Faisal <mfaisal@aucegypt.edu>
2020-05-11 17:53:09 +0200

    Initial WIFI moduel web interface

---
+++ ESP8266/ESP8266.ino
@@ -0,0 +1,125 @@
+// Load Wi-Fi library
+#include <ESP8266WiFi.h>
+
+const char* ssid     = "IIM";
+const char* password = "";
+
+WiFiServer server(80);
+
+String header;
+
+unsigned long currentTime = millis();
+unsigned long previousTime = 0;
+const long timeoutTime = 2000;
+
+String led3State = "On";
+String currentRTCReading = "00:00:00";
+
+void setup() {
+  Serial.begin(115200);
+
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  // Print local IP address and start web server
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  server.begin();
+}
+
+
+void sendString(String str){
+  for (int i = 0; i < str.length(); i++){
+     delay(1);
+     Serial.print(str[i]);
+     delay(1);
+    }
+  }
+
+void loop(){
+  WiFiClient client = server.available();   // Listen for incoming clients
+
+  if (client) {                             // If a new client connects,
+//    Serial.println("New Client.");           // print a message out in the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    currentTime = millis();
+    previousTime = currentTime;
+    while (client.connected() && currentTime - previousTime <= timeoutTime) { // loop while the client's connected
+      currentTime = millis();
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+//        Serial.write(c);                    // print it out the serial monitor
+        header += c;
+        if (c == '\n') {                    // if the byte is a newline character
+          if (currentLine.length() == 0) {
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+
+            // turns the GPIOs on and off
+            if (header.indexOf("GET /led/on") >= 0) {
+              sendString(";ATLED;");
+              led3State = "On";
+            } else if (header.indexOf("GET /led/off") >= 0) {
+              sendString(";ATLED;");
+              led3State = "Off";
+            } else if (header.indexOf("GET /time/refresh") >= 0) {
+              //currentRTCReading = Serial.readString();
+              // delay(5);
+              sendString(";ATTIME;");
+              delay(20);
+              currentRTCReading = Serial.readString();
+            }
+
+            // Display the HTML web page
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+
+
+            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+            client.println(".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;");
+            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+            client.println(".button2 {background-color: #77878A;}</style></head>");
+
+            client.println("<body><h1>Simple iot-app</h1>");
+
+            client.println("<p>Current Led3 State " + led3State + "</p>");
+            if (led3State=="Off") {
+              client.println("<p><a href=\"/led/on\"><button class=\"button\">TURN ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/led/off\"><button class=\"button button2\">TURN OFF</button></a></p>");
+            }
+
+            client.println("<p>Current Time: " + currentRTCReading + "</p>");
+            client.println("<p><a href=\"/time/refresh\"><button class=\"button button2\">Refresh Time</button></a></p>");
+            client.println("</body></html>");
+
+            client.println();
+            break;
+          } else {
+            currentLine = "";
+          }
+
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+//    Serial.println("Client disconnected.");
+//    Serial.println("");
+  }
+}
